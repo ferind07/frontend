ac3b3ee4bdb7d51e98b42ebc1de7df3a62b5758e
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-01-26 16:31:16 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+const App = () => {
+  return(
+    <React.Fragment>
+      <Navbar></Navbar>
+      <h2>Hello world</h2>
+    </React.Fragment>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+//css
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      
+    </nav>
+  )
+}
+
+export default Navbar;
