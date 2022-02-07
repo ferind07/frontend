7973a85deb73b14180ec19e79b53c4462e9619b9
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-02-07 12:24:29 +0700

    update frontend

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,20 +5,11 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <div className='nav-items flex-start'>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-          </ul>
-        </div>
-        <div className='nav-items flex-end'>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-          </ul>
-        </div>
-      </nav>
+      <header>
+        <nav>
+          <img src="/asset/image/logo.png" />
+        </nav>
+      </header>
     </>
   )
 }
