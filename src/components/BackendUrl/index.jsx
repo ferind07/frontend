4fcf9c7c60b0998014eac3c8e5f54f7cc4964820
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-06-22 18:50:13 +0700

    Update index.jsx

--- src/components/BackendUrl/index.jsx
+++ src/components/BackendUrl/index.jsx
@@ -1,3 +1,4 @@
-const BackendUrl = "https://api.ferryindragunawan.com";
+//const BackendUrl = "https://api.ferryindragunawan.com";
+const BackendUrl = "https://backend-zmdm.onrender.com";
 
 export default BackendUrl;
