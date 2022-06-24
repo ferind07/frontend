2b5910a89cf4166893d2fcc7917b9bab618f4594
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-06-24 07:19:40 +0700

    Update index.jsx

--- src/pages/InstructorRoutes/index.jsx
+++ src/pages/InstructorRoutes/index.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import NavbarInstructor from "../../components/NavbarInstructor";
 import RegisterInstructorPage from "../RegisterInstructorPage";
-//import InstructorPage from "../InstuctorPage";
-//import InstructorPage from "../InstuctorPage";
+import InstructorPage from "../InstuctorPage";
 import InstructorClass from "../InstructorClass";
 import InstructorSchedule from "../InstructorSchedule";
 import InstructorDetailSchedule from "../InstructorDetailSchedule/index.jsx";
@@ -27,7 +26,7 @@ const InstructorRoutes = () => {
       <Routes>
         <Route path="/" element={<RegisterInstructorPage />} />
         <Route path="/waiting" element={<InstructorWaiting />} />
-        {/* <Route path="/home" element={<InstructorPage />} /> */}
+        <Route path="/home" element={<InstructorPage />} />
         <Route path="/class" element={<InstructorClass />} />
         <Route path="/schedule" element={<InstructorSchedule />} />
         <Route path="/aboutMe" element={<InstructorAboutMe />} />
